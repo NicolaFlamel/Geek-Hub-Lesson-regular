192460dad29ac9a8422cebbca5bb1365346ad04a
NicolaFlamel <taurus93@ukr.net>
2013-11-21 20:23:06 +0200

    Квадратные скобки и циркумфлекс

--- javascript.js
+++ javascript.js
@@ -3,6 +3,9 @@
  */
 ;(function(){
 
-    console.log("q1we".match(/[wq\d]+/));//q1w
+    console.log("q1we".match(/[wq\d]+/g));//q1w
+    console.log("q1we".match(/[^wq\d]+/g));//1
+
    //[asd]-или а или с или d,[] воспринимается как один символ;
+    //[^] _в квадратных дужках ^ означает восклицание
 })();
