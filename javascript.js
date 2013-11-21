c636b7aaa6cbd73c891f4a6267d634525741ff6d
NicolaFlamel <taurus93@ukr.net>
2013-11-21 20:58:36 +0200

    Метод replace

--- javascript.js
+++ javascript.js
@@ -3,14 +3,6 @@
  */
 ;(function(){
 
-    /*console.log("max asd srgii".match(/max|srgii/));
- //  | - мне нужно , что бы была строка "мах" или "srgii"
-    console.log("max asd 3srgii".match(/\d(max|srgii)/g));
-    console.log("max 22asd 3srgii".match(/max|\d{2}srgii|\d{2}asd/g));//["max", "22asd"]
-*/
-    console.log(" solid 23px #aaa".match(/(\(?:d+|asd)px #(\w+)/));
-
-//?: - исключение из или |
-
-
+    console.log(" solid 23px #a23".replace(/\d+px #(\w+)/,'$1sergii'));//soild a23Sergii
+    //replace - меняет выбранную строчку слов
 })();
