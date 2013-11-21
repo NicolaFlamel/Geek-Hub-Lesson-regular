67b047b9bd086fcc1a6defea3aec04e790a80736
NicolaFlamel <taurus93@ukr.net>
2013-11-21 19:54:08 +0200

    Начало строки ^ ,\d-числа

--- javascript.js
+++ javascript.js
@@ -2,21 +2,18 @@
  * Created by Артем on 21.11.13.
  */
 ;(function(){
-//g-global возвращает все совпадения
-    //+ - 0 do infinite
-    console.log("asd\nwad".match(/.+/));
 
-
-    console.log("".match(/.+/)); //null
-    console.log("asd".match(/.+/)); //asd
-    console.log("".match(/.+/)); //null
-    console.log("asd".match(/./)); //a
-
-    console.log("".match(/.*/)); //pustaya stroka
-    console.log("asd".match(/.*/)); //asd
-
-    console.log("",match(/.?/)); //""
-    console.log("asd",match(/.?/)); //a
-
+    console.log("\nawdw".match(/^./));//null
+    console.log("\nawdw".match(/^./m));//a
+    /*m-в нашей строке есть переход на новую строку,
+     а значит m определяет переход на новую строку то
+     есть после каждого\n будет првоерять строку
+    */
+    console.log("asd\nawdw".match(/^\d/));//null
+    console.log("1\nasd\nawdw".match(/^\d/));//null
+    console.log("\nasd\n1wdw".match(/^\d/m));//1
+/*
+* \d ищет все числа
+* */
 
 })();
